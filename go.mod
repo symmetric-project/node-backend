6d3af40d5960df6003d9366fb999280d8aa44fa5
tschwann <tschwann@protonmail.com>
2021-09-07 19:43:37 +0300

    switch to graphql

--- go.mod
+++ go.mod
@@ -3,15 +3,7 @@ module github.com/symmetric-project/node-backend
 go 1.16
 
 require (
-	github.com/Masterminds/squirrel v1.5.0
-	github.com/georgysavva/scany v0.2.9
-	github.com/gin-contrib/cors v1.3.1
+	github.com/99designs/gqlgen v0.13.0
 	github.com/gin-gonic/gin v1.7.4
-	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/jackc/pgtype v1.8.1
-	github.com/jackc/pgx/v4 v4.13.0
-	github.com/joho/godotenv v1.3.0
-	github.com/pkg/errors v0.9.1
-	github.com/ztrue/tracerr v0.3.0
-	gopkg.in/guregu/null.v4 v4.0.0
+	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- main.go
+++ main.go
@@ -1,87 +1,32 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"os"
+	"github.com/symmetric-project/node-backend/graph"
+	"github.com/symmetric-project/node-backend/graph/generated"
 
-	sq "github.com/Masterminds/squirrel"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/joho/godotenv"
-	"github.com/pkg/errors"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-var MODE string
-var HTTPClient *http.Client
-var DATABASE_URL string
-var JWT_SECRET string
-
-var DBPool *pgxpool.Pool
-var SQ sq.StatementBuilderType
-
-var G *gin.Engine
-
-var COOKIE_DOMAIN string
-var COOKIE_SECURE bool
-
-func init() {
-	godotenv.Load()
-	MODE = os.Getenv("MODE")
-	HTTPClient = &http.Client{}
-	DATABASE_URL = os.Getenv("DATABASE_URL")
-	JWT_SECRET = os.Getenv("JWT_SECRET")
-
-	switch MODE {
-	case "dev":
-		gin.SetMode(gin.DebugMode)
-	case "prod":
-		gin.SetMode(gin.ReleaseMode)
-	default:
-		err := errors.New("unable to determine running mode (dev or prod)")
-		HandleError(err)
-		os.Exit(1)
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
+}
 
-	SQ = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
-	var err error
-	DBPool, err = pgxpool.Connect(context.Background(), DATABASE_URL)
-	if err != nil {
-		HandleError(err)
-		os.Exit(1)
-	}
-
-	jwt, err := GenerateBackdoorJWT()
-	if err != nil {
-		HandleError(err)
-		os.Exit(1)
-	}
-	log.Println("Backdoor JWT: " + jwt)
-
-	switch MODE {
-	case "dev":
-		COOKIE_DOMAIN = os.Getenv("COOKIE_DOMAIN_DEV")
-		COOKIE_SECURE = false
-	case "prod":
-		COOKIE_DOMAIN = os.Getenv("COOKIE_DOMAIN_PROD")
-		COOKIE_SECURE = true
-	default:
-		err := errors.New("unable to determine running mode (dev or prod)")
-		HandleError(err)
-		os.Exit(1)
+func graphqlPlaygroundHandler() gin.HandlerFunc {
+	h := playground.Handler("graphql", "/")
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
 func main() {
-	G = gin.Default()
-	G.Use(cors.New(cors.Config{
-		AllowAllOrigins:  false,
-		AllowOrigins:     []string{"http://symmetric.localhost:3000", "https://symmetric.nodename.com"},
-		AllowCredentials: true,
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Origin", "Cache-Control", "Credential"},
-	}))
-	G.Run(":4000")
+	r := gin.Default()
+	r.POST("/", graphqlHandler())
+	r.GET("/", graphqlPlaygroundHandler())
+	r.Run(":4000")
 }
